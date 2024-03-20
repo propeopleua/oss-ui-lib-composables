c9be067f256b79a81ac2b106ae5c67f639eed0b5
Andrii Muzyka <Andrii.Muzyka@pfizer.com>
2024-03-20 11:52:12 +0200

    - udpate build

--- src/list/useList.ts
+++ src/list/useList.ts
@@ -11,7 +11,7 @@ interface Props<T> {
 }
 
 interface ListConfig<T> {
-  form: FormInterface<T>;
+  form: FormInterface;
   tableSortHandler: (params: SortingState) => void;
   pageChangeHandler: (newPage: number) => void;
 }
